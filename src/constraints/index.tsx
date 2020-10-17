fa3c04253878721267d8aa4c104c7a09a7f80d52
Yuri Correa <yuri.correa1@outlook.com>
2020-10-17 12:32:00 -0300

    chore: add constraints with sizes,fonts and colors

---
+++ src/constraints/index.tsx
@@ -0,0 +1,21 @@
+export const colors = {
+  white: '#fff',
+  gray: '#f5f7fa',
+  black: '#243656',
+  primary: '#0070ba',
+  green: '#37d39b',
+  red: '#f47090',
+  blackWithOpacity: '#929BAB',
+  radialGradient: ['#0070ba', '#1546ac'],
+};
+
+export const sizes = {
+  small: 12,
+  regular: 16,
+  bold: 16,
+};
+
+export const fonts = {
+  ManropeRegular: 'Manrope-Regular',
+  ManropeSemiBold: 'Manrope-SemiBold',
+};
